20f2c150f71cd84385fe7422c4f563be51e7e58d
TheBoringDude <iamcoderx@gmail.com>
2021-05-18 10:45:30 +0800

    minor index page fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,23 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { NextPage } from 'next';
 
 import { PageLayout } from '@layouts/PageLayout';
-import { LinkButton } from '@components/shared/LinkButton';
 import { Container } from '@components/Container';
 import { Color } from '@components/shared/colorizer';
 
-export default function Index() {
-  const { user } = useUser();
-
+const Index: NextPage = () => {
   return (
     <PageLayout pageTitle="phurma | Form Submission Using an API">
       <section className="py-20 text-center">
         <Container size="w-4/5">
-          <h2 className="text-5xl font-black tracking-wide">
+          <h2 className="text-5xl font-black tracking-wide text-gray-800">
             Integrate <Color>Forms</Color> with an <Color>API</Color>
           </h2>
-          <p className="py-8 text-xl tracking-wide">
+          <p className="py-8 text-xl tracking-wide text-gray-700">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sit beatae molestiae
             doloremque. Soluta rem odio, quo earum sint quia.
           </p>
 
-          <div className="inline-flex mt-8">
+          <div className="inline-flex mt-8 text-gray-800">
             <button className="mx-1 py-3 px-8 rounded-lg tracking-wide font-bold text-xl border-purple-500 border hover:text-purple-500">
               Learn More
             </button>
@@ -32,4 +29,6 @@ export default function Index() {
       </section>
     </PageLayout>
   );
-}
+};
+
+export default Index;
