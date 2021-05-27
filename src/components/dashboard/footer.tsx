9bb843eaf4b3d6a48620019c29d5e8c851543817
TheBoringDude <iamcoderx@gmail.com>
2021-05-28 07:26:48 +0800

    setup footer links

--- src/components/dashboard/footer.tsx
+++ src/components/dashboard/footer.tsx
@@ -1,21 +1,27 @@
+import Link from 'next/link';
+
 export const DashFooter = () => {
   return (
     <>
       <hr />
       <footer className="w-5/6 mx-auto py-10 flex items-center justify-between">
-        <h3 className="font-bold tracking-wide text-purple-500">phurma</h3>
+        <Link href="/">
+          <a>
+            <h3 className="font-bold tracking-wide text-purple-500">phurma</h3>
+          </a>
+        </Link>
 
         <p className="text-sm text-gray-700">&copy; 2021 | All Rights Reserved</p>
 
         <ul className="text-sm text-gray-500">
           <li>
-            <a href="#">Github</a>
+            <a href="https://github.com/TheBoringDude/phurma">Github</a>
           </li>
           <li>
-            <a href="#">Quaker</a>
+            <a href="https://quaker.vercel.app">Quaker</a>
           </li>
           <li>
-            <a href="#">LCL Paste</a>
+            <a href="https://lcl-paste.vercel.app">LCL Paste</a>
           </li>
         </ul>
       </footer>
