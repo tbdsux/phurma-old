4f1fc0259b432b749223f932e1dc8c0faa9f2dd3
TheBoringDude <iamcoderx@gmail.com>
2021-06-11 21:03:35 +0800

    use `grid` in list-projects rendering

--- src/components/dashboard/list-projects.tsx
+++ src/components/dashboard/list-projects.tsx
@@ -26,18 +26,18 @@ export const ListProjects = ({ projects: initialProjects }: ListProjectProps) =>
   }
 
   return (
-    <ul className="w-11/12 mx-auto flex flex-col">
+    <ul className="w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4">
       {projects &&
         projects.data.map((project, index) => (
           <Link href={`/dashboard/projects/${project.data.id}`} key={index}>
             <a
               title={`Select project '${project.data.name}'`}
-              className="my-2 rounded-lg border group border-purple-300 hover:bg-purple-400 px-6 py-4 flex items-center justify-between"
+              className="my-2 rounded-lg border group border-purple-300 hover:bg-purple-400 px-6 py-4"
             >
               <strong className="text-lg tracking-wide font-bold text-gray-700 group-hover:text-white">
                 {project.data.name}
               </strong>
-              <p className="text-gray-400 tracking-wide font-medium group-hover:text-gray-100">
+              <p className="mt-2 text-right text-gray-400 tracking-wide font-medium group-hover:text-gray-100">
                 ({project.data.formRefs.length}{' '}
                 {`form${project.data.formRefs.length !== 1 ? 's' : ''}`})
               </p>
