41e0a785deeb2178c62f3a97a2276de38d4657eb
TheBoringDude <iamcoderx@gmail.com>
2021-07-13 07:00:17 +0800

    chore: fixes to development configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // Run type-check on changes to TypeScript files
   'src/**/*.ts?(x)': () => 'yarn type-check',
   // Run ESLint on changes to JavaScript/TypeScript files
-  'src/**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`
+  'src/**/*.(ts|js)?(x)': () => `yarn lint`
 };
